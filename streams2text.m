2f52b56267e7c069a7156b1859d509b5f5ad26ea
Zachary Roper <zachroper@gmail.com>
2017-05-30 15:04:16 -0500

    New python processing files added

---
+++ streams2text.m
@@ -0,0 +1,12 @@
+events = Cajal_data.evnt.scalars.EVNT.data;
+ts = 100*Cajal_data.evnt.scalars.EVNT.ts;
+
+FID = fopen('out.csv','w');
+
+[nrows,ncols] = size(events);
+for row = 1:ncols 
+    Save = [events(row); ts(row)];
+    fprintf(FID, '%d, %3.4f', Save);
+    fprintf(FID,'\n');
+end
+fclose(FID);
